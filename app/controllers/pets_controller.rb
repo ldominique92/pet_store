1a61e149a40e437da0839ae185fdf1b782f203b8
ldominique92 <larissadominique92@gmail.com>
2022-08-31 13:14:31 +0200

    complete crud

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,11 +4,36 @@ class PetsController < ApplicationController
   end
 
   def show
+    @pet = Pet.find(params[ :id])
   end
 
   def new
+    @pet = Pet.new
+  end
+ 
+  def create
+    @pet = Pet.new(params.require( :pet).permit( :name, :age, :description))
+    if @pet.save
+      redirect_to @pet
+    else 
+      render 'new'
+    end
   end
 
   def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(params.require( :pet).permit( :name, :age, :description))
+    redirect_to @pet
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+  
+    redirect_to pets_path
   end
 end
